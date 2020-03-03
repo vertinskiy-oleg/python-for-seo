1c5dbe3a1c46eadcbf1a6ce61cf11d210927e09c
vertinskiy-oleg <vertinskiy.o@gmail.com>
2020-03-03 12:37:14 +0200

    dz12

---
+++ dz12/timedeco.py
@@ -0,0 +1,11 @@
+def time_decorator(func):
+    def wrapper(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print('Completion time', t2 - t1)
+        with open('log.txt', 'a') as f:
+            f.write(f'{func.__name__} completion - {t2 - t1} sec\n')
+        return result
+
+    return wrapper

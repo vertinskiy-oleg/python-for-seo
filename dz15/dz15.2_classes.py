ee8f94ca8ad40523e15508e3b35dc4837859e4c2
vertinskiy-oleg <vertinskiy.o@gmail.com>
2020-03-11 19:02:52 +0200

    dz15

---
+++ dz15/dz15.2_classes.py
@@ -0,0 +1,21 @@
+# Website, Page, SearchEngine, Text, Title
+
+
+class SearchEngine:
+    pass
+
+
+class Website:
+    pass
+
+
+class Page(Website):
+    pass
+
+
+class Text:
+    pass
+
+
+class Title(Text):
+    pass
